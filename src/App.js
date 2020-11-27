cc7690057bc6614846f62f67f4c00e3d40e6a1dc
Oleg Voitiuk <bubmain25@gmail.com>
2020-11-27 17:02:47 +0200

    First changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="g-recaptcha brochure__form__captcha" data-sitekey="6Lfkpe8ZAAAAAOWGiHqgo3zqa2jEUA0ExiGn2aqp"></div>
     </div>
   );
 }
